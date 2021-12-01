5ce8149ab86fe518bea721d00aaf05a5507c4caf
Emmanuel Umoren <umoren.e1@gmail.com>
2021-12-01 12:39:55 +0100

    Workshop1done

---
+++ src/Componets/Table.js
@@ -0,0 +1,133 @@
+import React, { useState } from 'react';
+import { Fragment } from 'react';
+
+const Table=()=> {
+    const initailData =[
+        {id: 1, firstName: "Ruth", lastName: "Joe", age: 64, birthDay:"1963-03-05", country: "Maxico", city: "Maxicocity" },
+        {id: 2, firstName: "Danie", lastName: "Jame", age:74, birthDay:"1953-04-05", country: "USA", city: "Ohio" },
+        {id: 3, firstName: "Shawn", lastName: "Rice", age: 64, birthDay:"1973-03-07", country: "USA", city: "Califonia" },
+        {id: 4, firstName: "Ula", lastName: "Jacob", age: 26, birthDay:"1990-03-05", country: "Maxico", city: "Maxicocity" },
+        {id: 5, firstName: "Ruth", lastName: "Joe", age: 64, birthDay:"1963-03-05", country: "Maxico", city: "Maxicocity" },
+        {id: 6, firstName: "Ruth", lastName: "Joe", age: 64, birthDay:"1963-03-05", country: "Maxico", city: "Maxicocity" },
+        {id: 7, firstName: "Ruth", lastName: "Joe", age: 64, birthDay:"1963-03-05", country: "Maxico", city: "Maxicocity" }
+
+    ];
+
+
+    const [studentList, setStudentList] = useState(initailData);
+    const [showData, setShowData] = useState(false);
+    const studentDefaultData = {id: 0, firstName: "", lastName: "", age: 0, birthDay: "", country: "", city: "" };
+    const [student, setStudent] = useState(studentDefaultData);
+
+
+
+    const TableHeader = () =>{
+        return (
+            <thead className="table-light">
+                <td>id</td>
+                <td>FirstName</td>
+                <td>LastName</td>
+                <td>Age</td>
+                <td>Action</td>
+            </thead>
+        );
+    };
+
+
+
+
+    const TableRow = (props) =>{
+        return (
+            <tbody>
+                {
+                   props.studentList.map((student) =>{
+                       const { id, firstName, lastName, age } = student;
+                       return(
+                           <tr key={id}>
+                               <td>{id}</td>
+                               <td>{firstName}</td>
+                               <td>{lastName}</td>
+                               <td>{age}</td>
+                               <td><TableAction student={student}/></td>
+                           </tr>
+
+                    
+                       )
+                })
+            }
+            </tbody>
+            
+        )
+    }
+
+
+
+
+    const TableAction= ({student}) =>{
+        const showData= () =>{
+            setShowData(true);
+            setStudent(student);
+        
+        };
+        return (
+            <button type="button" className="btn btn-primary" onClick={showData} >Details</button>
+            
+        )
+    }
+
+
+
+    const ShowStudentDetails = () =>{
+        const { id, firstName, lastName, country, city,birthDay} = student;
+        return (
+            <Fragment>
+                {
+                    showData &&
+                    <div className="card" style= {{ width: '500px'}} > 
+                    <div className= "card-header bg-info text-white">
+                        Student information
+                    </div>
+                    <div className = "card-body">
+                        <h4 className="card-title"> {country}: {city}</h4>
+                        <p className= "card-text">ID: {id}</p>
+                        <p className= "card-text">Name: {firstName} {lastName}</p>
+                        <p className= "card-text">Birthday: {birthDay}</p>
+
+                    </div>
+                    <div className="card-footer">
+                        <button type="button" className="btn btn-info" onClick={() => {setShowData(false); setStudent(studentDefaultData) }}>Hide info</button>
+
+                    </div>
+                    
+
+                    </div>
+                }
+
+            
+            
+            </Fragment>
+           
+            
+        )
+    }
+
+
+
+    const TableChildren = ({children}) => <table className= "table table-striped">{children}</table>
+       
+    return (
+        <div className="container">
+            <h3> student List</h3>
+            <TableChildren>
+                <TableHeader/>
+                <TableRow studentList={studentList}/>
+            </TableChildren>
+            <br/>
+            <ShowStudentDetails/>
+
+            
+        </div>
+    );
+}
+
+export default Table;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import "bootstrap/dist/css/bootstrap.css";
-const Greeting = () => {
-  return <h1>Hello Coders!</h1>;
-}
-ReactDOM.render(<Greeting/>, document.getElementById('root'));
+import Table from './Componets/Table';
+// const Greeting = () => {
+//   return <h1>Hello Coders!</h1>;
+// }
+ReactDOM.render(<Table/>, document.getElementById('root'));
